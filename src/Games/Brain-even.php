9232928c266dc2e53b8200a4615b5c9f39978600
TurboMilfer <58418856+TurboMilfer@users.noreply.github.com>
2021-04-21 15:15:16 +0300

    Update Brain-even.php

--- src/Games/Brain-even.php
+++ src/Games/Brain-even.php
@@ -24,7 +24,7 @@ function isEven()
         if ($userResult === $result) {
             line("Correct!");
         } else {
-            line("'{$userResult}' is wrong answer ;(. Correct answer was '{$result}'.");
+            line("Let's try again, {$name}!");
             break;
         }
         if ($winCount === 3) {
